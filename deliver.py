a21fcaa59810aa1b00e867f5ea996f1ae05b4a68
Chris Harding <chris.harding@acm.org>
2011-12-12 04:08:22 +0000

    Automated email working with calibre-smtp

--- deliver.py
+++ deliver.py
@@ -2,13 +2,28 @@
 
 ''' Deliver all ebooks to the Kindle via e-mail '''
 
-from subprocess import call
+import subprocess
 import ConfigParser
+import shlex
 
 # Load config settings
 config = ConfigParser.ConfigParser()
 config.read('settings.cfg')
 
 
-call(["calibre-smtp", "-h"])
+# Build command list
+command_list = ['calibre-smtp',
+'-vv',
+'--attachment', 'ebooks/test.txt',
+'--subject', 'convert',
+'--password', config.get('Delivery', 'smtp_password'),
+'--port', config.get('Delivery', 'smtp_port'),
+'--relay', config.get('Delivery', 'smtp_server'),
+'--username', config.get('Delivery', 'smtp_username'),
+config.get('Delivery', 'smtp_username'),
+config.get('Delivery', 'kindle_address'),
+'Automated message sent from Calibre Kindle Server']
 
+
+# Send email using calibre's command line SMTP tool
+subprocess.call( command_list )
